3804163c0f04ca86ac7bf1a05e7ba1e4efc1000c
IsuriDisanayaka <isuriumeshika1@gmail.com>
2023-10-24 15:17:51 +0530

    #3 Feat:implement admin login in backend

--- news_api/api/db/server.js
+++ news_api/api/db/server.js
@@ -3,11 +3,14 @@ const cors = require('cors');
 const connectDB = require('./db');
 const express = require('express');
 
+const adminRouter = require('../routes/Admin');
+
 const PORT = process.env.PORT
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(adminRouter);
 
 
 connectDB();
---
+++ news_api/api/models/Admin.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema({
+
+  email: {
+    type: String,
+    required: [true, "Email is Required"],
+    unique: true,
+
+  },
+  password: {
+    type: String,
+    required: [true, "Password is Required"]
+  }
+});
+
+AdminSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+})
+AdminSchema.statics.login = async function (email, password) {
+
+  const admin = await this.findOne({ email });
+  console.log({ email, password });
+  if (admin) {
+    const auth = await bcrypt.compare(password, admin.password);
+    if (auth) {
+      return admin;
+    }
+    throw Error("incorrect password");
+  }
+  throw Error("Incorrect Email");
+  
+}
+module.exports = mongoose.model('Admin', AdminSchema);
